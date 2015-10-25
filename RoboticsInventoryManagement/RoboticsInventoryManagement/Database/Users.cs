8fc3aab5ec2efa2be301213955c18f078a81d12e
gzejnull <gzejnull@emich.edu>
2015-10-25 17:00:46 -0400

    camel cased everything

--- RoboticsInventoryManagement/RoboticsInventoryManagement/Database/Users.cs
+++ RoboticsInventoryManagement/RoboticsInventoryManagement/Database/Users.cs
@@ -8,8 +8,8 @@ namespace RoboticsInventoryManagement.Database
     public class Users
     {
         public int user_id { get; set; } //PK
-        public string fname { get; set; }
-        public string lname { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
         public string email { get; set; }
         public string password { get; set; }
         public int phoneNumber { get; set; }
