3b387cd5908bb43638cc015be879ad4681661d89
gzejnull <gzejnull@emich.edu>
2015-10-25 16:46:41 -0400

    Made user table

---
+++ RoboticsInventoryManagement/RoboticsInventoryManagement/Database/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoboticsInventoryManagement.Database
+{
+    public class Users
+    {
+        public int user_id { get; set; } //PK
+        public string fname { get; set; }
+        public string lname { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public int phoneNumber { get; set; }
+        public bool isAdmin { get; set; }
+    }
+}
