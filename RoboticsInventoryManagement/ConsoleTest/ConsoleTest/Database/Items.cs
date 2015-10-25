4a73312b15b3ae16624acd64fc286359788adc69
corecritter <bobbysavel@gmail.com>
2015-10-25 18:01:59 -0400

    Added Test Project

---
+++ RoboticsInventoryManagement/ConsoleTest/ConsoleTest/Database/Items.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RoboticsInventoryManagement.Database
+{
+    public class Items
+    {
+        public int ItemId { get; set; }
+        public int ItemTypeId { get; set; }
+        public int InventoryLocationId { get; set; }
+
+        public virtual ItemTypes ItemType { get; set; }
+        public virtual InventoryLocations InventoryLocation { get; set; }
+        public virtual Schools School{ get; set; }
+    }
+}
