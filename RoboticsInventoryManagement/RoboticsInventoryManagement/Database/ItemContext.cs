cdc87237a7d72e2fd667388559d073c5173b715c
Dnoorbak <Dnoorbak@emich.edu>
2015-10-25 17:56:06 -0400

    Added Locations to item context

--- RoboticsInventoryManagement/RoboticsInventoryManagement/Database/ItemContext.cs
+++ RoboticsInventoryManagement/RoboticsInventoryManagement/Database/ItemContext.cs
@@ -10,5 +10,6 @@ namespace RoboticsInventoryManagement.Database
     {
         public DbSet<ItemTypes> ItemTypes { get; set; }  //Entity Of Item Types
         public DbSet<Items> Items{ get; set; }           //Entity Of Items
+        public DbSet<InventoryLocations> InventoryLocations { get; set; }  //Entity Of Item Locations
     }
 }
