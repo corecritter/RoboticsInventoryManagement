a7018b454bcaa5e5297d9f27d9384a72a16f665b
gzejnull <gzejnull@emich.edu>
2015-11-01 16:30:34 -0500

    school context

---
+++ InventoryManagement/InventoryManagement/Database/SchoolsContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Database
+{
+    public class SchoolsContext
+    {
+        public DbSet<InventoryLocations> InventoryLocations { get; set; }
+    }
+}
