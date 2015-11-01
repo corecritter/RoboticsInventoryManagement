8bd5d839f39646a50ebb4854e1035a4956ed6b31
gzejnull <gzejnull@emich.edu>
2015-11-01 16:55:22 -0500

    school controller

--- InventoryManagement/InventoryManagement/App_Start/RouteConfig.cs
+++ InventoryManagement/InventoryManagement/App_Start/RouteConfig.cs
@@ -17,8 +17,9 @@ namespace InventoryManagement
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 //defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
+                //defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             // defaults: new { controller = "ItemTypes", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Schools", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- InventoryManagement/InventoryManagement/Database/SchoolsContext.cs
+++ InventoryManagement/InventoryManagement/Database/SchoolsContext.cs
@@ -6,8 +6,10 @@ using System.Web;
 
 namespace InventoryManagement.Database
 {
-    public class SchoolsContext
+    public class SchoolsContext : DbContext
     {
         public DbSet<InventoryLocations> InventoryLocations { get; set; }
+
+        public System.Data.Entity.DbSet<InventoryManagement.Database.Schools> Schools { get; set; }
     }
 }
