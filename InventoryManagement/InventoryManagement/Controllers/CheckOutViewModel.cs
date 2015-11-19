5696b2e6f732ac12877587261ea0bfd69f9439fe
gzejnull <gzejnull@emich.edu>
2015-11-18 19:27:01 -0500

    here you go Q

---
+++ InventoryManagement/InventoryManagement/Controllers/CheckOutViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InventoryManagement.Database;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace InventoryManagement.Controllers
+{
+    public class CheckOutViewModel
+    {
+        public Schools SchoolModel { get; set; }
+
+        [Display(Name = "Schools")]
+        public IList<Schools> Schools { get; set; };
+    }
+}
