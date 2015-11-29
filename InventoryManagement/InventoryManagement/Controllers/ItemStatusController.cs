932fa37d597e2bd8ba40601ab8e94aadcf834fcf
corecritter <bobbysavel@gmail.com>
2015-11-29 04:34:50 -0500

    Added Status Page

---
+++ InventoryManagement/InventoryManagement/Controllers/ItemStatusController.cs
@@ -0,0 +1,27 @@
+﻿using InventoryManagement.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagement.Models;
+
+namespace InventoryManagement.Controllers
+{
+    public class ItemStatusController : Controller
+    {
+        private ItemContext db = new ItemContext();
+        public ActionResult Index()
+        {
+            var allItems = db.Items.Where(item => item.CheckedOutById != null);         //Items checked out or returned
+            var checkedOutItems = allItems.Where(item => item.CheckedInById == null);  //Items checked out
+            var pendingApprovalItems = allItems.Where(item => item.CheckedInById != null); //Items checked out and in
+            ItemStatusViewModel vm = new ItemStatusViewModel
+            {
+                CheckedOutItems = checkedOutItems.ToList(),
+                PendingApprovalItems = pendingApprovalItems.ToList()
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement/Models/ItemStatusViewModel.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagement.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Models
+{
+    public class ItemStatusViewModel
+    {
+        public IList<Items> CheckedOutItems { get; set; }
+        public IList<Items> PendingApprovalItems { get; set; }
+    }
+}
