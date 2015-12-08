4929bdf81d95d03c1bceb400eedeb8a15433ae0b
corecritter <bobbysavel@gmail.com>
2015-12-07 22:03:41 -0500

    Added Schools Page

---
+++ InventoryManagement/InventoryManagement/Controllers/SchoolItemsController.cs
@@ -0,0 +1,40 @@
+﻿using InventoryManagement.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManagement.Models;
+
+namespace InventoryManagement.Controllers
+{
+    public class SchoolItemsController : Controller
+    {
+        private ItemContext db = new ItemContext();
+        public ActionResult Index()
+        {
+            SchoolsItemsIndexViewModel vm = new SchoolsItemsIndexViewModel
+            {
+                Schools = db.Schools.OrderBy(school => school.SchoolName).ToList()
+            };
+            return View(vm);
+        }
+        public ActionResult ViewItems(int? id)
+        {
+            if(id==null)
+                return RedirectToAction("Index");
+
+            var selectedSchool = db.Schools.Find(id);
+            if (selectedSchool == null)
+                return RedirectToAction("Index");
+            var rentedItems = db.Items.Where(item => item.CheckedOutSchoolId == id);
+            rentedItems = rentedItems.Where(item => item.CheckedOutById != null);
+            rentedItems = rentedItems.Where(item => item.CheckedInById == null);
+            SchoolsItemsViewModel vm = new SchoolsItemsViewModel
+            {
+                CheckedOutItems = rentedItems.OrderBy(item => item.ItemType.ItemName).ToList()
+            };
+            return View(vm);
+        }
+    }
+}
---
+++ InventoryManagement/InventoryManagement/Models/SchoolsItemsIndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using InventoryManagement.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventoryManagement.Models
+{
+    public class SchoolsItemsIndexViewModel
+    {
+        public IList<Schools> Schools { get; set; }
+    }
+    public class SchoolsItemsViewModel
+    {
+        public IList<Items> CheckedOutItems { get; set; }
+    }
+}
